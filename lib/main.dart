15986f9bb1de7678f2250fe6114094ff15451170
Alain MK <alainmukingiro.am@gmail.com>
2024-10-31 12:04:10 +0100

    feat: first commit with the initial configurations

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flaconi_weather/theme/theme.dart';
+import 'package:flaconi_weather/weather/ui/weather_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flaconi Weather',
+      theme: FlaconiTheme.light(),
+      debugShowCheckedModeBanner: false,
+      home: const WeatherScreen(),
+    );
+  }
+}
